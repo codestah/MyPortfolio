03755619654592a5e6213060c377961b6237aff8
Codestah <s3465614@student.rmit.edu.au>
2017-06-24 19:37:47 +1000

    added angular and ruby  custom scopes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+3.times do |topic|
+	Topic.create!(
+		title: "Topic #{topic}"
+	)
+end
+
+puts "3 topics create"
+
 10.times do |blog|     
 	Blog.create!(         
 		title: "My Blog Post #{blog}",
@@ -8,7 +16,8 @@ owned fo shizzle doggy. Maurizzle mah nizzle nibh et fizzle. Yo mamma izzle
 tortizzle. Pellentesque dang we gonna chung izzle. In fo shizzle habitasse
 platea dictumst. The bizzle dapibizzle. Curabitur the bizzle my shizz,
 pretizzle eu, mattizzle funky fresh, eleifend da bomb, nunc. Own yo
-suscipizzle. Boofron sizzle velit shiznit ."     
+suscipizzle. Boofron sizzle velit shiznit .",
+topic_id: Topic.last.id     
 	) 
 end  
 puts "10 blog posts created"
@@ -21,10 +30,27 @@ puts "10 blog posts created"
 end 
 puts "5 skills created"
 
-9.times do |portfolio_item| 
+8.times do |portfolio_item| 
 	Portfolio.create!(     
 		title: "Portfolio title: #{portfolio_item}",
-		subtitle: "My service",     
+		subtitle: "Ruby on Rails",     
+		body: "Lorizzle yippiyo cool sit amizzle, own yo
+adipiscing elit. Nullam sapizzle velizzle, owned volutpizzle, funky fresh,
+check out this vel, fo. Pellentesque away tortor. Sed erizzle. Uhuh ... yih!
+sizzle dolor dapibizzle owned fo shizzle doggy. Maurizzle mah nizzle nibh et
+fizzle. Yo mamma izzle tortizzle. Pellentesque dang we gonna chung izzle. In
+fo shizzle habitasse platea dictumst. The bizzle dapibizzle. Curabitur the
+bizzle my shizz, pretizzle eu, mattizzle funky fresh, eleifend da bomb, nunc.
+Own yo suscipizzle. Boofron sizzle velit shiznit ." ,     
+		main_image: "http://via.placeholder.com/600x400",     
+		thumb_image: "http://via.placeholder.com/350x200" 
+	) 
+end 
+
+1.times do |portfolio_item| 
+	Portfolio.create!(     
+		title: "Portfolio title: #{portfolio_item}",
+		subtitle: "Angular",     
 		body: "Lorizzle yippiyo cool sit amizzle, own yo
 adipiscing elit. Nullam sapizzle velizzle, owned volutpizzle, funky fresh,
 check out this vel, fo. Pellentesque away tortor. Sed erizzle. Uhuh ... yih!
