df92dc7b9b93a959b87f29650af9f1f12dbeb6b0
Codestah <s3465614@student.rmit.edu.au>
2017-06-25 19:08:06 +1000

    Implemented authorization engine for portfolio items and blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,6 +5,13 @@ class BlogsController < ApplicationController
                                   :destroy, 
                                   :toggle_status]
   layout "blog"
+  access all: [:show, :index], 
+          user: {except: [:destroy, 
+                          :new, 
+                          :create, 
+                          :update, 
+                          :edit]}, 
+          site_admin: :all
   
   # GET /blogs
   # GET /blogs.json
