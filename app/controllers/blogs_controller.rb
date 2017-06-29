6877f44dce9ad110adcc484507510ac9821156e4
Codestah <s3465614@student.rmit.edu.au>
2017-06-29 17:28:34 +1000

    Implemented custom scope for recent blogs and hide draft posts from non admins

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,16 +17,21 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.page(params[:page]).per(5)
+    if logged_in?(:site_admin)
+      @blogs = Blog.recent.page(params[:page]).per(5)
+    else
+      @blogs = Blog.published.recent.page(params[:page]).per(5)
+    end
     @page_title = "My Portfolio Blog"
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
-  def show
+  def show  
     @blog = Blog.includes(:comments).friendly.find(params[:id])
     @comment = Comment.new
-    
+
+  
     @page_title = @blog.title
     @seo_keywords = @blog.body
   end
@@ -58,7 +63,7 @@ class BlogsController < ApplicationController
 
   # PATCH/PUT /blogs/1
   # PATCH/PUT /blogs/1.json
-  def update
+  def update 
     respond_to do |format|
       if @blog.update(blog_params)
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
